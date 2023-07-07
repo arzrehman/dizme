0923d6f5abebd2e8e3a019cd509d87971e613ea6
Rehman <rehman.pk95@gmail.com>
2023-07-07 18:16:09 +0500

    w

--- js/geo.js
+++ js/geo.js
@@ -1,49 +1,50 @@
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(successCallback);
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  } else {
+    // Geolocation not supported
+    redirectToMainPage();
   }
   
   function successCallback(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-  
-    // Make an AJAX request to a geolocation API to get the country information
+    
+    // Make an AJAX request to the GeoNames API to get the country information
     var xhr = new XMLHttpRequest();
     var url = 'http://api.geonames.org/findNearbyJSON?lat=' + latitude + '&lng=' + longitude + '&username=arehman';
-  
+    
     xhr.open('GET', url, true);
     xhr.onreadystatechange = function() {
       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
         var response = JSON.parse(xhr.responseText);
-        var countryCode = response.country_code;
-  
-        // Use the country code to redirect the user
+        var countryCode = response.countryCode;
+        
+        // Redirect the user based on the country code
         if (countryCode === 'US') {
-          window.location.href = '/us.html';
+          redirectToCountryPage('/us.html');
         } else if (countryCode === 'UK') {
-          window.location.href = '/uk.html';
+          redirectToCountryPage('/uk.html');
         } else if (countryCode === 'PK') {
-          window.location.href = '/pk.html';
+          redirectToCountryPage('/pk.html');
+        } else {
+          redirectToMainPage();
         }
       }
     };
-  
+    
     xhr.send();
-  }
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-  } else {
-    // Geolocation is not supported
-    // Handle the scenario accordingly (e.g., display a default page)
-  }
-  
-  function successCallback(position) {
-    // Your existing code to handle the successful geolocation retrieval
-    // ...
   }
   
   function errorCallback(error) {
-    // Handle the scenario when the user denies access to their location
-    // Redirect the user to a default page or display a message
-    // ...
+    // Handle geolocation error
+    redirectToMainPage();
+  }
+  
+  function redirectToCountryPage(pageUrl) {
+    window.location.href = pageUrl;
+  }
+  
+  function redirectToMainPage() {
+    window.location.href = '/index.html';
   }
   
